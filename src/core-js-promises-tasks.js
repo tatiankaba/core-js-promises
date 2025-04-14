22918c247e3036b5049fc62e551f8a3089ba78ea
Tatiana <bodtata@yandex.ru>
2025-04-14 08:48:08 +0200

    feat: solve task 7

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -145,8 +145,17 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  let result = '';
+  return promises
+    .reduce((acc, curr) => {
+      return acc
+        .then(() => curr)
+        .then((value) => {
+          result += value;
+        });
+    }, Promise.resolve())
+    .then(() => result);
 }
 
 module.exports = {
